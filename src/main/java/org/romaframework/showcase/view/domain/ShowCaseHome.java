06c709bc1560a445a18e57d78f1f08eff66d72a2
maggiolo00 <maggiolo00@precisionbox.(none)>
2012-10-23 22:45:37 +0200

    added first mock of accordion render

--- src/main/java/org/romaframework/showcase/view/domain/ShowCaseHome.java
+++ src/main/java/org/romaframework/showcase/view/domain/ShowCaseHome.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.romaframework.aspect.core.annotation.AnnotationConstants;
 import org.romaframework.aspect.view.ViewConstants;
 import org.romaframework.aspect.view.annotation.ViewField;
+import org.romaframework.showcase.view.domain.component.Accordion;
 import org.romaframework.showcase.view.domain.component.Form;
 import org.romaframework.showcase.view.domain.component.RenderRowSet;
 import org.romaframework.showcase.view.domain.component.Table;
@@ -14,14 +15,15 @@ import org.romaframework.showcase.view.domain.menu.BackOfficeMenu;
 public class ShowCaseHome {
 
 	@ViewField(render = ViewConstants.RENDER_TAB, selectionField = "selected", label = "")
-	private Map<String, Object>	tabs	= new HashMap<String, Object>();
+	private Map<String, Object> tabs = new HashMap<String, Object>();
 	@ViewField(visible = AnnotationConstants.FALSE)
-	private Object							selected;
+	private Object selected;
 
 	public ShowCaseHome() {
 		tabs.put("Form", new Form());
 		tabs.put("Table", new Table());
 		tabs.put("Menu", new BackOfficeMenu());
+		tabs.put("Accordion", new Accordion());
 		tabs.put("Rowset", new RenderRowSet(ViewConstants.RENDER_ROWSET));
 		tabs.put("Colset", new RenderRowSet(ViewConstants.RENDER_COLSET));
 	}
